7c5380ae5c3044d1afab29811861eafdb4350d43
dasstyx <dasstyx@gmail.com>
2023-02-25 03:36:09 +0300

    add photos handling

--- src/botik_telebot/input/message_handlers/raw_message_handlers.py
+++ src/botik_telebot/input/message_handlers/raw_message_handlers.py
@@ -1,12 +1,16 @@
 import logging
+import telebot
 
 from botik.input.message_handlers.events import events
 from botik.input.message_handlers.raw_message_handlers import RawMessageHandlers
+
+from botik_telebot.api.attachment_handlers.photo_attachment_handler import PhotoAttachmentHandler
 
 
 class RawMessageHandlers(RawMessageHandlers):
     def _initialize_handlers(self, bot):
         bot.message_handler(content_types=['text'])(self.message_reply)
+        bot.message_handler(content_types=['photo'])(self.photo_reply)
         bot.message_handler(content_types=['contact'])(self.phone_reply)
         bot.message_handler(content_types=['location'])(self.location_reply)
         bot.message_handler(commands=['start'])(self.start_reply)
@@ -31,6 +35,13 @@ class RawMessageHandlers(RawMessageHandlers):
 
         await user.storage.set("location", location)
         await events.geo_share(user, location)
+
+    async def photo_reply(self, message):
+        user = await self._get_user_from_message(message)
+        msg_photos = message.photo
+        handler = PhotoAttachmentHandler(self.bot)
+        photo = await handler.from_server(msg_photos)
+        await events.got_attachment(user, photo)
 
     async def phone_reply(self, message):
         user = await self._get_user_from_message(message)
